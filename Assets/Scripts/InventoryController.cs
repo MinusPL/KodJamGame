46bd7b615bacec4207b497cd1171154f7c1a2177
JustDante <34778875+JustDante@users.noreply.github.com>
2020-12-31 11:45:00 +0100

    finaled

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -12,7 +12,7 @@ public class InventoryController : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         slots = new List<GameObject>();
         for(int i = 0; i < slotCount; i++)
